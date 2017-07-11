9cbaf306b20822ac5ca540275ef809b58e97ed3e
Mike Gazzaruso <mike@devoloop.com>
2017-07-11 17:56:27 +0200

    Fixed Issues

--- DynaRageTest/ViewController.swift
+++ DynaRageTest/ViewController.swift
@@ -7,19 +7,48 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
+    
+    var rageProcessor: AKDynaRageCompressor!
+    
+    let input = AKStereoInput()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        rageProcessor = AKDynaRageCompressor(input)
+        AudioKit.output = rageProcessor
+        
+        AudioKit.start()
+        
+        setupUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func setupUI() {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.addArrangedSubview(AKPropertySlider(
+            property: "Rage Amount",
+            format: "%0.2f s",
+            value: self.rageProcessor.rageAmount, minimum: 0.1, maximum: 20.0,
+            color: UIColor.green) { sliderValue in
+                self.rageProcessor.rageAmount = sliderValue
+        })
+        
+        view.addSubview(stackView)
+        
+        stackView.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.9).isActive = true
+        stackView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.9).isActive = true
+        
+        stackView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        stackView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
-
-
 }
 
